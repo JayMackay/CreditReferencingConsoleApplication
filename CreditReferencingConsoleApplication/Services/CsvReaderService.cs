3e45e4c01068965711039156833ccde423392dd1
JaredMackay <JaredMackay@paypoint.com>
2024-07-05 13:46:48 +0100

    Create a CsvReaderService that handles all file handling logic

---
+++ CreditReferencingConsoleApplication/Services/CsvReaderService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CreditReferencingConsoleApplication.Interfaces;
+using CreditReferencingConsoleApplication.Models;
+
+namespace CreditReferencingConsoleApplication.Services
+{
+    public class CsvReaderService : ICsvReaderService
+    {
+        public List<Transaction> ReadTransactions(string filePath)
+        {
+            var transactions = new List<Transaction>();
+
+            try
+            {
+                using(var reader = new StreamReader(filePath))
+                using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+
+                    while(csv.Read())
+                    {
+                        var transaction = csv.GetRecord<Transaction>();
+                        transactions.Add(transaction);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error reading transactions CSV file: {ex.Message}");
+            }
+
+            return transactions;
+        }
+
+        public List<Property> ReadProperties(string filePath)
+        {
+            var properties = new List<Property>();
+
+            try
+            {
+                using(var reader = new StreamReader(filePath))
+                using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+
+                    while(csv.Read())
+                    {
+                        var property = csv.GetRecord<Property>();
+                        properties.Add(property);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error reading properties CSV file: {ex.Message}");
+            }
+
+            return properties;
+        }
+    }
+}
